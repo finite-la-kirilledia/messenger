81b136a45b4c9ba0668982701953a6c049a9c4b5
Kirill Minko <kirill040994@gmail.com>
2020-09-25 00:04:38 +0300

    initial commit

---
+++ app/src/main/java/com/finite_la_kirilledia/messenger/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.finite_la_kirilledia.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
