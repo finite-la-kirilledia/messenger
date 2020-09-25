c40fc73f9dda6fd7ce2d7b90d72856ecab5403d7
Kirill Minko <kirill040994@gmail.com>
2020-09-25 19:26:25 +0300

    create Login and Register screens

---
+++ app/src/main/java/com/finite_la_kirilledia/messenger/login/LoginFragment.kt
@@ -0,0 +1,13 @@
+package com.finite_la_kirilledia.messenger.login
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.finite_la_kirilledia.messenger.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/finite_la_kirilledia/messenger/register/RegisterFragment.kt
@@ -0,0 +1,13 @@
+package com.finite_la_kirilledia.messenger.register
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.finite_la_kirilledia.messenger.R
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
