4b00e11373c9638e3332befce92fa2c34b81d355
Kirill Minko <kirill040994@gmail.com>
2020-09-25 21:23:11 +0300

    implement logout

--- app/src/main/java/com/finite_la_kirilledia/messenger/chat_list/ChatListFragment.kt
+++ app/src/main/java/com/finite_la_kirilledia/messenger/chat_list/ChatListFragment.kt
@@ -1,13 +1,35 @@
 package com.finite_la_kirilledia.messenger.chat_list
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.finite_la_kirilledia.messenger.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ChatListFragment : Fragment(R.layout.fragment_chat_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_logout -> {
+                Firebase.auth.signOut()
+                findNavController().navigate(ChatListFragmentDirections.actionChatListFragmentToLoginFragment())
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
